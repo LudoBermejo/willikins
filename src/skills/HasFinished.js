9d192a4b38efcb2ab9c70c4888e0fc22ad4841d2
Ludo Bermejo <ludobermejo@gmail.com>
2016-11-27 13:57:13 +0100

    New version

--- src/skills/HasFinished.js
+++ src/skills/HasFinished.js
@@ -1,4 +1,4 @@
 const detection = require('./../utils/detection')
 module.exports = function (skill, info, bot, message) {
-  detection(skill, info, bot, message, 'hasfinished');
+  return detection(skill, info, bot, message, 'hasfinished');
 };
